3a530934ebff67161e606f70e6e91d66422b3e33
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-06-02 21:58:35 +0200

    delete

--- src/main/java/classes/copies/Pies.java
+++
@@ -1,5 +0,0 @@
-package classes.copies;
-
-public class Pies {
-    public String name;
-}
--- src/main/java/overload/OverloadProgram.java
+++
@@ -1,8 +0,0 @@
-package overload;
-
-public class OverloadProgram {
-    public static void main(String[] args) {
-        Overload overload
-                = new Overload(1);
-    }
-}
