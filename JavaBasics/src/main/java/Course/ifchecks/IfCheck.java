cf10d1432e014ec6e2d7e7e63a0c7e983233af90
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-06-03 09:07:36 +0200

    new

---
+++ JavaBasics/src/main/java/Course/ifchecks/IfCheck.java
@@ -0,0 +1,28 @@
+package ifchecks;
+
+public class IfCheck {
+
+    public static void main(String[] args) {
+
+        int i = 5;
+        if (i < 5) {
+            System.out.println("i jest mniejsze niż 5");
+        } else {
+            System.out.println("i nie jest mniejsze niż 5");
+        }
+
+        i = 3;
+        if (i == 1) {
+            System.out.println("i jest równe 1");
+            i = 5;
+        } else if (i == 2) {
+            System.out.println("i jest równe 2");
+        } else if (i == 3) {
+            System.out.println("i jest równe 3");
+        } else {
+            System.out.println("i jest większe niż 3");
+        }
+
+        System.out.println("KONIEC");
+    }
+}
