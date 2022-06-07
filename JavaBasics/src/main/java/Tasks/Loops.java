914c4d3fd72d270f75c3f6a01a6e1991af8c3c9d
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-06-07 09:59:07 +0200

    new

--- JavaBasics/src/main/java/Tasks/Loops.java
+++ JavaBasics/src/main/java/Tasks/Loops.java
@@ -1,2 +1,28 @@
-package Tasks;public class Loops {
+package Tasks;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        Loops loop = new Loops();
+        loop.Loop1();
+
+        int j = 0;
+        while (j <= 10 ) {
+            int y = j * 3;
+            System.out.println(j + ": y = " + y);
+            j++;
+        }
+        int k = 0;
+        do {
+            int y = k * 3;
+            System.out.println(k + ": y = " + y);
+            k++;
+        } while (k <= 10 );
+    }
+    public void Loop1() {
+        for (int i = 0; i <= 10; i++) {
+            int y = i * 3;
+            System.out.println(i + ": y = " + y);
+        }
+    }
 }
