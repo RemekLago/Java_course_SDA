5849e0f5f5e1108e9734d5532b0ce6b30ff8494c
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-06-10 08:04:16 +0200

    new

--- JavaAdvance/src/main/java/part1/NewPerson.java
+++ JavaAdvance/src/main/java/part1/NewPerson.java
@@ -1,10 +1,21 @@
-package Course2;
+package part1;
 
-public class newPerson{
+public class NewPerson{
     public String name;
-    public int date;
+    public int age;
+    public char sex;
+
+    public NewPerson(){
+    }
+
+    public NewPerson(String name, int age, char sex){
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
 
     public void introduce(){
-        System.out.println("My name is " + this.name + ", I'm " + this.date + " years old");
+        System.out.println("My name is " + this.name + ", I'm " + this.age + " years old");
     }
+
 }
