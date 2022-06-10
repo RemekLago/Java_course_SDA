ee90b5a428bb6d91550239bdc642decee3d6e15d
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-06-10 08:03:58 +0200

    new

--- JavaAdvance/src/main/java/part1/BankAccountMain.java
+++ JavaAdvance/src/main/java/part1/BankAccountMain.java
@@ -1,2 +1,16 @@
-package part1;public class BankAccountMain {
+package part1;
+
+public class BankAccountMain {
+    public static void main(String[] args) {
+        BankAccount accountAndrzej = new BankAccount(123L, 1000);
+        BankAccount accountBeata = new BankAccount(555L, 2000);
+
+        accountAndrzej.BalanceAccount();
+        accountAndrzej.paymentAccount(500);
+        accountAndrzej.BalanceAccount();
+        accountAndrzej.withdrawAccount(300);
+        accountAndrzej.BalanceAccount();
+
+        accountAndrzej.getBankAccount();
+    }
 }
