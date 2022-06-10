140d08015badb94edd7123d1d0c5c80c1457beb8
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-06-10 08:04:54 +0200

    new

--- JavaAdvance/src/main/java/part2/Fraction.java
+++ JavaAdvance/src/main/java/part2/Fraction.java
@@ -1,2 +1,44 @@
-package part2;public class Fractions {
+package part2;
+
+public class Fraction {
+
+    private int nominator;
+    private int denominator;
+
+    public Fraction() {
+    }
+
+    public Fraction(int nominator, int denominator) {
+        this.nominator = nominator;
+        this.denominator = denominator;
+    }
+
+    public void multiplication(Fraction other){
+        this.nominator *= other.nominator;
+        this.denominator *= other.denominator;
+        System.out.println("Result of multiplication is: " + this.nominator + "/" + this.denominator);
+    }
+
+    public void division(int x, int y){
+        this.nominator *= y;
+        this.denominator *= x;
+        System.out.println("Result of division is: " + this.nominator + "/" + this.denominator);
+    }
+
+    public void addition(int x, int y){
+        this.nominator = (nominator*y + denominator*x);
+        this.denominator = (denominator*y);
+        System.out.println("Result of addition is: " + this.nominator + "/" + this.denominator);
+    }
+
+    public void subtraction(int x, int y){
+        this.nominator = (nominator*y - denominator*x);
+        this.denominator = (denominator*y);
+        System.out.println("Result of subtraction is: " + this.nominator + "/" + this.denominator);
+    }
+
+    public String toString(){
+        return nominator + "/" + denominator;
+    }
+
 }
