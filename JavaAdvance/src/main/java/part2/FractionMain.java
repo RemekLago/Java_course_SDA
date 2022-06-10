6f91c3da0ee1449f6cf15d50b1efd26958fd684a
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-06-10 08:05:05 +0200

    new

--- JavaAdvance/src/main/java/part2/FractionMain.java
+++ JavaAdvance/src/main/java/part2/FractionMain.java
@@ -1,2 +1,18 @@
-package part2;public class FractionsMain {
+package part2;
+
+public class FractionMain {
+    public static void main(String[] args) {
+        Fraction a = new Fraction (1, 2);
+        Fraction b = new Fraction (4, 9);
+        System.out.println(a);
+
+        a.multiplication(b);
+        System.out.println(a);
+        a.division(1, 2);
+        System.out.println(a);
+        a.addition(1, 2);
+        System.out.println(a);
+        a.subtraction(1, 2);
+        System.out.println(a);
+    }
 }
