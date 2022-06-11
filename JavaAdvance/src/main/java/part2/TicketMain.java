c4a12c7c67529c5f7e542e8618720568d7d68653
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-06-11 08:14:56 +0200

    new

--- JavaAdvance/src/main/java/part2/TicketMain.java
+++ JavaAdvance/src/main/java/part2/TicketMain.java
@@ -1,2 +1,20 @@
-package part2;public class TicketMain {
+package part2;
+
+public class TicketMain {
+    public static void main(String[] args) {
+        Ticket ticket1 = Ticket.NORMALNY_CALODNIOWY;
+        Ticket ticket2 = Ticket.NORMALNY_GODZINOWY;
+        Ticket ticket3 = Ticket.ULGOWY_CALODNIOWY;
+        Ticket ticket4 = Ticket.ULGOWY_GODZINNY;
+
+        System.out.println(ticket1.getPrice());
+        System.out.println(ticket2.getPrice());
+        System.out.println(ticket3.getPrice());
+        System.out.println(ticket4.getPrice());
+
+        ticket1.informationTicket();
+
+        Ticket result = Ticket.whichTicket(20, 90, 80);
+        System.out.println("Ticket result: " + result.name());
+    }
 }
