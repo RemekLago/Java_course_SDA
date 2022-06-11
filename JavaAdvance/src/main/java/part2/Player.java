db1e2c53e77a22d887b9f7e175632fe7fedbd583
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-06-11 08:14:13 +0200

    new

--- JavaAdvance/src/main/java/part2/Player.java
+++ JavaAdvance/src/main/java/part2/Player.java
@@ -1,9 +1,31 @@
 package part2;
 
-public class Race {
-
-}
-
 public class Player{
+    private final String name;
+    public final int id;
+    private int minSpeed;
+    private int maxSpeed;
+    private int distance;
+
+    public Player(String name, int id, int minSpeed, int maxSpeed) {
+        this.name = name;
+        this.id = id;
+        this.minSpeed = minSpeed;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void introduce(){
+        System.out.println("My name is " + this.name + ", my number is " + this.id + ".");
+    }
+    public double runTime(double length){
+        double time = length / ((this.maxSpeed + this.minSpeed)/2);
+        System.out.println("Player number " + this.id + ": distance " + length + " km in " + String.format("%.2f", time) + " hours");
+        return time;
+    }
+    public void runDistance(double length){
+        double time = length / ((this.maxSpeed + this.minSpeed)/2);
+        double finalDistance = time * ((this.maxSpeed + this.minSpeed)/2);
+        System.out.println("Player number " + this.id + ": distance " + finalDistance + " km in " + String.format("%.2f", time) + " hours");
+    }
 
 }
