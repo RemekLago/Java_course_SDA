1b151f59f0c5abc0bc35664cf472f6c565761a74
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-06-12 08:47:03 +0200

    new

---
+++ JavaAdvance/src/main/java/part4/MyCircle.java
@@ -0,0 +1,19 @@
+package Part4;
+
+public class MyCircle extends Circle {
+
+//    impossible to override PI
+//    public void OverridePI(){
+//    protected final double Pi = 3.9;}
+
+
+    public MyCircle(final int radius) {
+        super(radius);
+    }
+
+    //    impossible to override
+//    @Override
+//    public final double areaCircle(){
+//        return radius * radius + 2;
+    }
+
