a465b70357ad45b11957adca13c76e91e62a08ae
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-06-14 07:56:28 +0200

    new

---
+++ JavaAdvance/src/main/java/part5/Calling/Calling.java
@@ -0,0 +1,9 @@
+package Part5.Calling;
+
+public interface Calling {
+    static int alarmNumber = 912;
+
+    public abstract void callingAnyNumber();
+
+    public abstract void callingAlarmNumber();
+}
