59ffd7b0873015a084ab582373d8b2282d437ede
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-06-15 07:47:11 +0200

    new

---
+++ JavaAdvance/src/main/java/part5/Calling/CallingMain.java
@@ -0,0 +1,12 @@
+package Part5.Calling;
+
+public class CallingMain {
+    public static void main(String[] args) {
+
+        Phone phone1 = new Phone("987-432-923");
+        Phone phone2 = new Phone("657-309-172");
+
+        phone1.callingAnyNumber();
+
+    }
+}
