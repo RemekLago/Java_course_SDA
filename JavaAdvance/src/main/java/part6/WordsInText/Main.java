0a76032705a8b403841dc3862310a9688a6e941b
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-07-06 07:58:52 +0200

    new

---
+++ JavaAdvance/src/main/java/part6/WordsInText/Main.java
@@ -0,0 +1,11 @@
+package part6.WordsInText;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String sentence = "dom kot kot lampa krzeslo, biurko plynie kot, " +
+                "czyta robi dom kot pies biurko lampa. kot.";
+
+        CountWordsInText.countingWordsInText(sentence);
+    }
+}
