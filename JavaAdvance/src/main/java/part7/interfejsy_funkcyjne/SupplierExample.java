7ffd925e077726e2db08079f9950de28abe67db5
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-07-07 07:56:46 +0200

    new

---
+++ JavaAdvance/src/main/java/part7/interfejsy_funkcyjne/SupplierExample.java
@@ -0,0 +1,18 @@
+package part7.interfejsy_funkcyjne;
+
+import java.util.function.Supplier;
+
+public class SupplierExample {
+
+  public static void main(String[] args){
+
+    getStringValue(() -> "cos ktos");
+
+  }
+
+
+  static void getStringValue(Supplier<String> supplier){
+    System.out.println(supplier.get());
+  }
+
+}
