8d98f663ee572e3119e696570a57d60da4bb3e33
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-07-08 08:25:24 +0200

    new

---
+++ JavaAdvance/src/main/java/part7/lambdy/KlasaPokaz.java
@@ -0,0 +1,35 @@
+package part7.lambdy;
+
+public class KlasaPokaz {
+
+  public static void main(String[] args){
+
+
+    wypiszDanyObiekt(new Wypisywalny() {
+      @Override
+      public void wypisz(){
+
+      }
+    });
+
+
+    // programisci to skroca do:
+    wypiszDanyObiekt(() -> System.out.println("jakas wartosc"));
+
+
+
+
+  }
+
+
+
+
+
+
+
+
+  public static void wypiszDanyObiekt(Wypisywalny obiekt){
+    obiekt.wypisz();
+  }
+
+}
