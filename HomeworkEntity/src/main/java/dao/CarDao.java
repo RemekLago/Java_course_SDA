60cf9887c985e01eaf929a181d848b399d13831b
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-07-09 11:09:02 +0200

    new

---
+++ HomeworkEntity/src/main/java/dao/CarDao.java
@@ -0,0 +1,32 @@
+package dao;
+
+import entity2.Car;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class CarDao extends GenericDao<Car>{
+    public CarDao(EntityManagerFactory factory, Class<Car> clazz) {
+        super(factory, clazz);
+    }
+
+    public List<Car> findPriceGreater(BigDecimal limit){
+        final EntityManager em = factory.createEntityManager();
+        final List<Car> cars = em.createQuery("select c from Car c where c.price > :limit", Car.class)
+                .setParameter("limit", limit)
+                .getResultList();
+        em.close();
+        return cars;
+    }
+
+    public List<String> findAllModels(){
+        final EntityManager em = factory.createEntityManager();
+        final List<String> models = em
+                .createQuery("select distinct c.model from Car c", String.class)
+                .getResultList();
+        em.close();
+        return models;
+    }
+}
