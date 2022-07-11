d71a8e5c44f6a6e32c438ccaa7ebc08d58a7a6e0
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-07-11 08:18:26 +0200

    new

---
+++ JavaAdvance/src/test/java/Testing/CalculatorTest.java
@@ -0,0 +1,22 @@
+package Testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void shouldAddTwoNumbers() {
+        // given
+        double valueA = 5.2;
+        double valueB = 3.1;
+        Calculator calculator = new Calculator();
+
+        // when
+        double actualResult = calculator.add(valueA, valueB);
+
+        // then
+        assertEquals(8.3, actualResult);
+    }
+}
