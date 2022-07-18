661b379cf70e5b7bcb81f35b1a3951ccd4745489
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-07-18 07:50:52 +0200

    new

---
+++ JavaAdvance/src/main/java/Tasks/Task1/NumberMain.java
@@ -0,0 +1,11 @@
+package Tasks.Task1;
+
+import Tasks.Task1.Number;
+
+public class NumberMain {
+    public static void main(String[] args) {
+
+       Number.finalNumber(Number.changingNumber(Number.splittingNumber(Number.userNumber())));
+
+    }
+}
