448907a48eb2957ef7f587a28408c8b306c7fba8
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-07-20 08:13:34 +0200

    new

---
+++ JavaAdvance/src/main/java/Tasks/Task3a/Square.java
@@ -0,0 +1,12 @@
+package Tasks.Task3a;
+
+public class Square extends Rectangle{
+
+    public Square(double width, double length) {
+        super(width, length);
+    }
+
+    public Square(double width, double length, String color, boolean isFilled) {
+        super(width, length, color, isFilled);
+    }
+}
