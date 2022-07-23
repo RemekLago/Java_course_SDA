9a6c2503e6b57eff7f4470da29d7aa81c98cae7a
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-07-23 08:51:02 +0200

    new

---
+++ JavaAdvance/src/main/java/Tasks/Task4a/Square1.java
@@ -0,0 +1,12 @@
+package Tasks.Task4a;
+
+public class Square1 extends Rectangle1 {
+
+    public Square1(double width, double length) {
+        super(width, length);
+    }
+
+    public Square1(double width, double length, String color, boolean isFilled) {
+        super(width, length, color, isFilled);
+    }
+}
