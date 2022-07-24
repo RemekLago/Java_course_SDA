72a41f1c1246a3e4f4066704169d58de312a731a
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-07-24 09:04:13 +0200

    new

---
+++ JavaAdvance/src/main/java/scooterrent/controller/MenuController.java
@@ -0,0 +1,24 @@
+package scooterrent.controller;
+
+import java.util.Scanner;
+
+public class MenuController {
+    private final Scanner scanner;
+    private final Menu menu;
+
+    public MenuController(Scanner scanner, Menu menu) {
+        this.scanner = scanner;
+        this.menu = menu;
+    }
+
+    public void processLoop(){
+        while(true){
+            menu.print();
+            if (scanner.hasNextInt()){
+                int option = scanner.nextInt();
+                scanner.nextLine();
+                menu.run(option);
+            }
+        }
+    }
+}
