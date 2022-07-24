dcae0d6cdbd1a1612a30e064433f3f44fcbf5bd4
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-07-24 09:00:57 +0200

    new

---
+++ JavaAdvance/src/main/java/scooterrent/entity/Rent.java
@@ -0,0 +1,37 @@
+package scooterrent.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "rents")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Rent extends BaseEntity {
+
+    private LocalDateTime startOn;
+
+    private LocalDateTime endOn;
+
+    private BigDecimal pricePerMinute;
+
+    private BigDecimal totalCost;
+
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Scooter scooter;
+}
