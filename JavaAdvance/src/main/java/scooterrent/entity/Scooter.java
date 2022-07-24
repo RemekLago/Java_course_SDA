c83cb99af6a281f01e5c7d063d5b9bdacca212d6
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-07-24 09:01:08 +0200

    new

---
+++ JavaAdvance/src/main/java/scooterrent/entity/Scooter.java
@@ -0,0 +1,24 @@
+package scooterrent.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "scooters")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(callSuper = true)
+public class Scooter extends BaseEntity {
+
+    private String identifier;
+
+    private String model;
+
+    @Builder.Default
+    private boolean available = true;
+}
