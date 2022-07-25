3a4a298d666f13578e59c6ae3fb3b9b027ccb21f
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-07-25 08:11:42 +0200

    new file

---
+++ SDA-Spring-core-di-xml/src/main/java/pl/sda/dao/impl/MockPersonDao.java
@@ -0,0 +1,24 @@
+package pl.sda.dao.impl;
+
+import pl.sda.dao.PersonDao;
+import pl.sda.model.Person;
+
+import java.util.List;
+
+public class MockPersonDao implements PersonDao {
+
+    @Override
+    public Person getById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Person> getAll() {
+        return null;
+    }
+
+    @Override
+    public Person add(Person person) {
+        return null;
+    }
+}
