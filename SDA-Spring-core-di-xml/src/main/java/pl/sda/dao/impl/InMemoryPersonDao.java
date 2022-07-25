63e8fb49661384c665083b199ebb063612040768
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-07-25 08:10:59 +0200

    new file

---
+++ SDA-Spring-core-di-xml/src/main/java/pl/sda/dao/impl/InMemoryPersonDao.java
@@ -0,0 +1,40 @@
+package pl.sda.dao.impl;
+
+import pl.sda.dao.PersonDao;
+import pl.sda.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class InMemoryPersonDao implements PersonDao{
+
+        private List<Person> personList;
+
+        public InMemoryPersonDao(){
+            personList = new ArrayList<>();
+            personList.add(new Person(1, "Michal", "Nowak"));
+            personList.add(new Person(2, "Anna", "Nowak"));
+            personList.add(new Person(3, "Jan", "Kowalski"));
+        }
+
+    @Override
+    public Person getById(Integer id){
+        return personList.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    @Override
+    public List<Person> getAll(){
+        return personList;
+    }
+
+    @Override
+    public Person add(Person person) {
+        personList.add(person);
+        return person; // jeśli chcę zwrócić listę to jest blą∂
+    }
+}
+
