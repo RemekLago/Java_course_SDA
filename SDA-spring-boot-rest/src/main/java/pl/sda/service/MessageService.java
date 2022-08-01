5229037c8d567189fda059a00ac19fe3e59d4a02
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-08-01 12:20:00 +0200

    new file

---
+++ SDA-spring-boot-rest/src/main/java/pl/sda/service/MessageService.java
@@ -0,0 +1,19 @@
+package pl.sda.service;
+
+import pl.sda.model.Message;
+
+import java.util.List;
+
+public interface MessageService {
+
+    void addMessage(Message message);
+
+    List<Message> getAllMessages();
+
+    void updateMessage(Message message);
+
+    void deleteMessage(Integer id);
+
+    Message getMessageById(Integer id);
+
+}
