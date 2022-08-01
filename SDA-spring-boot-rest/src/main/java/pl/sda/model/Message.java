cb1f9a0895c8f39adc9890122a90d43baf203932
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-08-01 12:19:09 +0200

    new file

---
+++ SDA-spring-boot-rest/src/main/java/pl/sda/model/Message.java
@@ -0,0 +1,18 @@
+package pl.sda.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+
+    private Integer id;
+
+    private String text;
+
+//    private String priority;
+
+}
