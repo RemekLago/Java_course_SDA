a55ec4951d24b31266672fc68e1f0c04b2a1df25
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-08-05 12:10:50 +0200

    new file

---
+++ solid/src/main/java/sda/arpjava/practice/FuelTank.java
@@ -0,0 +1,15 @@
+package sda.arpjava.practice;
+
+public class FuelTank extends Tank{
+    public FuelTank(double capacity) {
+        super(capacity);
+    }
+
+    public boolean isEmpty(){
+        return level() == 0;
+    }
+
+    public double space(){
+        return capacity - level();
+    }
+}
