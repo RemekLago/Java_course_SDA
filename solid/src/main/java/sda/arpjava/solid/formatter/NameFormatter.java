0d860928d58fb0a6e1eac973cf9f08e1cbdc2e3b
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-08-07 08:56:41 +0200

    new file

---
+++ solid/src/main/java/sda/arpjava/solid/formatter/NameFormatter.java
@@ -0,0 +1,8 @@
+package sda.arpjava.solid.formatter;
+
+public class NameFormatter {
+
+    public static String formatAsSelfName(String name) {
+        return name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+    }
+}
