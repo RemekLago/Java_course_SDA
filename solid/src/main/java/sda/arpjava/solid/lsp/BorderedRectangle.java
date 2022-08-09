ed4cf31a706eb8d3e4632967ffda423b425295cb
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-08-09 07:58:14 +0200

    new file

---
+++ solid/src/main/java/sda/arpjava/solid/lsp/BorderedRectangle.java
@@ -0,0 +1,14 @@
+package sda.arpjava.solid.lsp;
+
+public class BorderedRectangle extends Rectangle{
+    protected double stroke;
+
+    public BorderedRectangle(double width, double height) {
+        super(width, height);
+    }
+
+    public BorderedRectangle(double width, double height, double stroke) {
+        super(width, height);
+        this.stroke = stroke;
+    }
+}
