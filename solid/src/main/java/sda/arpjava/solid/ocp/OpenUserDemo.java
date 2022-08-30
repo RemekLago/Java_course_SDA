3f0e190af22b78432cb2e5a6faf8a21775cc56dc
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-08-30 07:15:17 +0200

    new file

---
+++ solid/src/main/java/sda/arpjava/solid/ocp/OpenUserDemo.java
@@ -0,0 +1,18 @@
+package sda.arpjava.solid.ocp;
+
+import sda.arpjava.solid.srp.User;
+
+public class OpenUserDemo {
+    public static void main(String[] args) {
+        //Drugi sposób rozszerzenia klasy User
+        //wykorzystanie konstruktora z parametrem formattera powoduje, że klasa jest otwarta na rozszerzenia
+        //własnych formaterów bez stosowania dzedziczenia
+        User user = new User("adam","kowal","1234","adam@sda.pl",
+                input -> input.substring(0,2).toUpperCase() + input.substring(2).toLowerCase()
+        );
+
+    }
+}
+
+
+
